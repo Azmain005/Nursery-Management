13cd6b53c6594c5c7558484a578bb9189b2f012c
Azmain005 <azmain.adib.arnob@gmail.com>
2025-04-20 19:05:57 +0600

    fixed the footer and made it slightly responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,22 +13,24 @@ const Footer = () => {
   return (
     <div
       className="
-        w-full 
-        bg-[#faf6e9] 
-        border-t-2 border-[#3e5931] 
-        px-10 py-20 
-        flex justify-evenly items-center 
+  w-full
+        bg-[#faf6e9]
+        border-t-2 border-[#3e5931]
+        px-10 py-20
+        gap-3
+        flex flex-col items-center justify-center
+        md:flex-row md:flex-wrap md:gap-5 md:justify-evenly md:items-center
         text-[#3e5931]
-        bg-cover 
+        bg-cover
       "
       style={{
         backgroundImage: `url(${footer}), url(${footer})`,
-        backgroundPosition: 'left, right-bottom',
-        backgroundRepeat: 'no-repeat, no-repeat',
-        backgroundSize: '40% auto, 40% auto',
+        backgroundPosition: "left, right-bottom",
+        backgroundRepeat: "no-repeat, no-repeat",
+        backgroundSize: "40% auto, 40% auto",
       }}
     >
-      <div className="flex flex-col items-start border-2 border-[#3e5931] rounded-lg bg-[#9bab9a] p-5 mr-10 w-[244px] h-[403px]">
+      <div className="flex flex-col items-start border-none rounded-xl bg-[#9bab9a]/60 backdrop-blur-md backdrop-filter p-5 mr-10 w-[244px] h-[403px]">
         <h1 className="text-2xl mb-5">Planty</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam,
@@ -37,22 +39,26 @@ const Footer = () => {
           maiores, unde a magni iusto sequi et obcaecati dignissimos iste ipsam.
         </p>
       </div>
+
       {/* 2nd */}
-      <div className="flex flex-col justify-between items-start border-2 border-[#3e5931] rounded-lg bg-[#607b64] text-white p-5 mr-10 w-[244px] h-[403px]">
+      <div className="flex flex-col justify-between items-start border-none rounded-xl bg-[#607b64]/90 backdrop-blur-md backdrop-filter text-white p-5 mr-10 w-[244px] h-[403px]">
         <div className="flex flex-col items-start">
           <h1 className="text-2xl mb-5">Contact Us</h1>
+
           <div className="mb-3">
             <div className="flex gap-1 items-center justify-center hover:text-black cursor-pointer">
               <AiOutlineEnvironment className="text-xl" />
               <p className="text-xs">BRAC University</p>
             </div>
           </div>
+
           <div className="mb-3">
             <div className="flex gap-1 items-center justify-center hover:text-black cursor-pointer">
               <AiFillPhone className="text-xl" />
               <p className="text-xs">01718740241</p>
             </div>
           </div>
+
           <div className="mb-3">
             <div className="flex gap-1 items-center justify-center hover:text-black cursor-pointer">
               <HiOutlineMail className="text-xl" />
@@ -60,6 +66,7 @@ const Footer = () => {
             </div>
           </div>
         </div>
+
         <div className="flex justify-between w-2/3 h-[40px] text-[25px] items-end">
           <Link className="hover:text-black">
             <IoLogoFacebook />
@@ -75,27 +82,54 @@ const Footer = () => {
           </Link>
         </div>
       </div>
+
       {/* 3rd */}
-      <div className="flex flex-col items-start border-2 border-[#3e5931] rounded-lg bg-[#607b64] p-5 mr-10 w-[244px] h-[403px] text-white text-sm">
+      <div className="flex flex-col items-start border-none rounded-xl bg-[#607b64]/80 backdrop-blur-md backdrop-filter p-5 mr-10 w-[244px] h-[403px] text-white text-sm">
         <h1 className="text-2xl mb-2 border-b-2 w-full pb-1">Categories</h1>
-        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">Fresh Fruits</p>
-        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">Bonsai Plants</p>
-        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">Botanical</p>
-        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">Office Plant</p>
-        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">Organic</p>
-        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">Seasonal Plants</p>
-        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">Special Plants</p>
-        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">Vegetables</p>
-        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">Latest</p>
-        
+        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">
+          Fresh Fruits
+        </p>
+        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">
+          Bonsai Plants
+        </p>
+        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">
+          Botanical
+        </p>
+        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">
+          Office Plant
+        </p>
+        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">
+          Organic
+        </p>
+        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">
+          Seasonal Plants
+        </p>
+        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">
+          Special Plants
+        </p>
+        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">
+          Vegetables
+        </p>
+        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">
+          Latest
+        </p>
       </div>
-      <div className="flex flex-col items-start border-2 border-[#3e5931] rounded-lg bg-[#607b64] p-5 mr-10 w-[244px] h-[403px] text-white text-sm">
+      {/* 4th */}
+      <div className="flex flex-col items-start border-none rounded-xl bg-[#607b64]/80 backdrop-blur-md backdrop-filter p-5 mr-10 w-[244px] h-[403px] text-white text-sm">
         <h1 className="text-2xl mb-2 border-b-2 w-full pb-1">Services</h1>
-        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">About Us</p>
-        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">Contact Us</p>
-        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">Privacy Policy</p>
-        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">Delivery Information</p>
-        
+
+        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">
+          About Us
+        </p>
+        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">
+          Contact Us
+        </p>
+        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">
+          Privacy Policy
+        </p>
+        <p className="border-b-2 w-full pb-1 pt-2 hover:translate-x-2 hover:text-black cursor-pointer hover:border-black">
+          Delivery Information
+        </p>
       </div>
     </div>
   );
